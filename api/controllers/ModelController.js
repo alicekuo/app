5b9d0f95239a96cd596fef121534046504b80362
alicekuo <alicekuo1119@gmail.com>
2015-12-12 15:51:29 +0800

    test ...

---
+++ api/controllers/ModelController.js
@@ -0,0 +1,21 @@
+/**
+ * ModelController
+ *
+ * @description :: Server-side logic for managing Models
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+
+
+  /**
+   * `ModelController.action()`
+   */
+  action: function (req, res) {
+    return res.json({
+      todo: 'action() is not implemented yet!'
+    });
+  }
+};
+
